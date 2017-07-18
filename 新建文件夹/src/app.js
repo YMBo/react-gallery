8cfcbe633d8879cb4e306d2d4f9fafc689ebfd21
YMB <619697451@qq.com>
2017-07-18 18:32:32 +0800

    预览

--- 新建文件夹/src/app.js
+++
@@ -1,8 +0,0 @@
-var React =require("react");
-var reactDom =require("react-dom");
-var GalleryByReact=require("./components/gallery/gallery.js");
-reactDom.render(
-	<GalleryByReact />,
-	document.getElementById("content")
-)
-
